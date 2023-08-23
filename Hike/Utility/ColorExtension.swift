024779bec0063de430353323d9525ac90361aef0
Wishtrader <mr.akomarov@gmail.com>
2023-08-23 13:15:05 +0300

    initial commit

---
+++ Hike/Utility/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Andrei Kamarou on 6.08.23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let customGreenLight = Color("ColorGreenLight")
+  static let customGreenMedium = Color("ColorGreenMedium")
+  static let customGreenDark = Color("ColorGreenDark")
+  static let customGreyLight = Color("ColorGrayLight")
+  static let customGreyMedium = Color("ColorGrayMedium")
+  static let customIndigoMedium = Color("ColorIndigoMedium")
+  static let customSalmonLight = Color("ColorSalmonLight")
+}
